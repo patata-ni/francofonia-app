3652e12c3b18ccc1665f76ff4e58ff24c52933b3
Tania-wq <23610066@utgz.edu.mx>
2026-03-26 20:07:37 -0600

    fix: elimina duplicados de use y rutas en web.php

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,6 @@ Route::post('/crear-scanner', [ScannerUserController::class, 'store'])->name('sc
 // Ruta para crear usuario admin desde el login
 Route::get('/crear-admin', [AdminUserController::class, 'showCreateForm'])->name('admin.create');
 Route::post('/crear-admin', [AdminUserController::class, 'store'])->name('admin.create.post');
-use App\Http\Controllers\AdminUserController;
-// Ruta para crear usuario admin desde el login
-Route::get('/crear-admin', [AdminUserController::class, 'showCreateForm'])->name('admin.create');
-Route::post('/crear-admin', [AdminUserController::class, 'store'])->name('admin.create.post');
 /*
 |--------------------------------------------------------------------------
 | Archivo: routes/web.php
